fea84722f75ecd720193652963d679cf294ba7d9
Makoto Kaneko <kaneko.m@tohnichi.co.jp>
2021-05-04 23:50:29 +0900

    プロジェクト ファイルを追加します。

---
+++ FTPSample/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace FTPSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonSelect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextUserName = new System.Windows.Forms.TextBox();
+            this.TextPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextFileName = new System.Windows.Forms.TextBox();
+            this.ButtonUpload = new System.Windows.Forms.Button();
+            this.TextHost = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ButtonSelect
+            // 
+            this.ButtonSelect.Location = new System.Drawing.Point(318, 96);
+            this.ButtonSelect.Name = "ButtonSelect";
+            this.ButtonSelect.Size = new System.Drawing.Size(62, 20);
+            this.ButtonSelect.TabIndex = 0;
+            this.ButtonSelect.Text = "Select";
+            this.ButtonSelect.UseVisualStyleBackColor = true;
+            this.ButtonSelect.Click += new System.EventHandler(this.ButtonSelect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // TextUserName
+            // 
+            this.TextUserName.Location = new System.Drawing.Point(76, 44);
+            this.TextUserName.Name = "TextUserName";
+            this.TextUserName.Size = new System.Drawing.Size(123, 20);
+            this.TextUserName.TabIndex = 3;
+            this.TextUserName.Text = "xuser";
+            // 
+            // TextPassword
+            // 
+            this.TextPassword.Location = new System.Drawing.Point(76, 70);
+            this.TextPassword.Name = "TextPassword";
+            this.TextPassword.Size = new System.Drawing.Size(123, 20);
+            this.TextPassword.TabIndex = 4;
+            this.TextPassword.Text = "xpass";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "File Name";
+            // 
+            // TextFileName
+            // 
+            this.TextFileName.Location = new System.Drawing.Point(76, 97);
+            this.TextFileName.Name = "TextFileName";
+            this.TextFileName.Size = new System.Drawing.Size(236, 20);
+            this.TextFileName.TabIndex = 6;
+            // 
+            // ButtonUpload
+            // 
+            this.ButtonUpload.Location = new System.Drawing.Point(134, 123);
+            this.ButtonUpload.Name = "ButtonUpload";
+            this.ButtonUpload.Size = new System.Drawing.Size(120, 20);
+            this.ButtonUpload.TabIndex = 7;
+            this.ButtonUpload.Text = "Upload";
+            this.ButtonUpload.UseVisualStyleBackColor = true;
+            this.ButtonUpload.Click += new System.EventHandler(this.ButtonUpload_Click);
+            // 
+            // TextHost
+            // 
+            this.TextHost.Location = new System.Drawing.Point(76, 18);
+            this.TextHost.Name = "TextHost";
+            this.TextHost.Size = new System.Drawing.Size(123, 20);
+            this.TextHost.TabIndex = 9;
+            this.TextHost.Text = "localhost";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(11, 21);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(52, 13);
+            this.label.TabIndex = 8;
+            this.label.Text = "FTP Host";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.TextHost);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.ButtonUpload);
+            this.Controls.Add(this.TextFileName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TextPassword);
+            this.Controls.Add(this.TextUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ButtonSelect);
+            this.Name = "Form1";
+            this.Text = "ftp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonSelect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextUserName;
+        private System.Windows.Forms.TextBox TextPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TextFileName;
+        private System.Windows.Forms.Button ButtonUpload;
+        private System.Windows.Forms.TextBox TextHost;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ FTPSample/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace FTPSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog folderBrowserDialog = new OpenFileDialog();
+            if (folderBrowserDialog.ShowDialog(this) != DialogResult.OK)
+            {
+                return;
+            }
+            else
+            {
+                TextFileName.Text = folderBrowserDialog.FileName;
+            }
+        }
+
+        private void ButtonUpload_Click(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("ftp://" + TextHost.Text + "/" + Path.GetFileName(TextFileName.Text));
+
+            FtpWebRequest ftpReq = (FtpWebRequest)WebRequest.Create(uri);
+            ftpReq.Credentials = new NetworkCredential(TextUserName.Text, TextPassword.Text);
+            ftpReq.Method = WebRequestMethods.Ftp.UploadFile;
+
+            // 要求の完了後(アップロード終了時)に接続を閉じる
+            // Connection closing at finished upload.
+            ftpReq.KeepAlive = false;
+            // ASCIIモードで転送する
+            // ASCII transfer enabled.
+            ftpReq.UseBinary = false;
+            // PASSIVEモードを有効にする(ファイアウォールの内側から外側にあるFTPサーバに接続する場合はTrueにする)
+            // PASSIVE enabled. PASSIVE enabled case is able to connecting server in the outside firewall.
+            ftpReq.UsePassive = true;
+
+            using (FileStream fs = new FileStream(TextFileName.Text, FileMode.Open, FileAccess.Read))
+            using (Stream reqStrm = ftpReq.GetRequestStream())
+            {
+                byte[] buffer = new byte[1024];
+                while (true)
+                {
+                    int readSize = fs.Read(buffer, 0, buffer.Length);
+                    if (readSize == 0)
+                    {
+                        break;
+                    }
+                    reqStrm.Write(buffer, 0, readSize);
+                }
+            }
+
+            MessageBox.Show("Upload success.");
+        }
+    }
+}
